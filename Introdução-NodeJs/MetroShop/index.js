b44e2e1b7c8f789a5e1a7e20f239d8a983268461
DESKTOP-7M01SED\Aluno <aluno@jaboatao.ifpe.edu.br>
2023-10-30 15:47:16 -0300

    produtos

--- Introdução-NodeJs/MetroShop/index.js
+++ Introdução-NodeJs/MetroShop/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use(express.static('public'));
 const PORT = 3000;
 
 app.get('/', (req, res) => {
@@ -7,12 +8,33 @@ app.get('/', (req, res) => {
 });
 
 produtos = [
-    { codigo: 1, nome: 'Fone de Ouvido', preco: 219.56 },
-    { codigo: 2, nome: 'Caneca', preco: 7.12 },
-    { codigo: 3, nome: 'Mochila', preco: 120 }];
+    { codigo: 1, nome: 'Fone de Ouvido', preco: 10.0 },
+    { codigo: 2, nome: 'Faca da Ana Maria Braga', preco: 5.0 },
+    { codigo: 3, nome: 'Sonzinho da JBL', preco: 20 }];
 
 app.get('/produtos', (req, res) => {
-    res.send(produtos)
+
+    var nomeFiltro = req.query.nome;
+    var precoMaximo = req.query.preco;
+
+    var resultadodaBusca = [];
+    produtos.forEach(produto => {
+        if (produto.nome.includes(nomeFiltro) &&
+            produto.preco <= precoMaximo) {
+            resultadodaBusca.push(produto);
+        }
+    })
+    res.send(resultadodaBusca);
+
+    var todosProdutos = '';
+    for (var i = 0; i < produtos.length; i++) {
+        todosProdutos += "Código: " + produtos[i].codigo;
+        todosProdutos += "Nome: " + produtos[i].nome;
+        todosProdutos += "Preço: " + produtos[i].preco;
+        todosProdutos += "<br>";
+
+    }
+    res.send(todosProdutos) 
 });
 
 app.listen(PORT, () => {
